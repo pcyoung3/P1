dac595e150fc37e5652cd39cc2525523377f8297
pcyoung <pcy35549@gmail.com>
2024-04-15 08:08:01 +0900

    [박치영] Player Default Pawn 설정

--- Source/P1/P1GameMode.cpp
+++ Source/P1/P1GameMode.cpp
@@ -5,5 +5,7 @@
 
 AP1GameMode::AP1GameMode()
 {
-	
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/BP_P1Player"));
+	if (PlayerPawnBPClass.Class != NULL)
+		DefaultPawnClass = PlayerPawnBPClass.Class;
 }
