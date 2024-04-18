15201290857402a0e94ee829dc9d8a9da9532005
pcyoung <pcy35549@gmail.com>
2024-04-19 08:31:57 +0900

    [pcyoung] animinstance 생성

---
+++ Source/P1/Animation/P1PlayerAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "P1PlayerAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class P1_API UP1PlayerAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
