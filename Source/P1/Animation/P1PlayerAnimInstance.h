fee8dce7e67e9d0754cf4a510eee39dcf1b67eb3
pcyoung <pcy35549@gmail.com>
2024-04-20 20:46:40 +0900

    [pcyoung] animinstance 수정 중

--- Source/P1/Animation/P1PlayerAnimInstance.h
+++ Source/P1/Animation/P1PlayerAnimInstance.h
@@ -14,4 +14,13 @@ class P1_API UP1PlayerAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="References", meta=(AllowPrivateAccess="true"))
+	TObjectPtr<class ACharacter> Character;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="References", meta=(AllowPrivateAccess="true"))
+	TObjectPtr<class UCharacterMovementComponent> CharacterMovementComponent;
 };
