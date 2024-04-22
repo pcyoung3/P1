897600a5cf73d1b43f6d342aec2e96cc07aec2c4
pcyoung <pcy35549@gmail.com>
2024-04-22 22:18:24 +0900

    [pcyoung] Animinstance 초안 완성

--- Source/P1/Animation/P1PlayerAnimInstance.cpp
+++ Source/P1/Animation/P1PlayerAnimInstance.cpp
@@ -3,3 +3,32 @@
 
 #include "Animation/P1PlayerAnimInstance.h"
 
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UP1PlayerAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	
+	Character = Cast<ACharacter>(TryGetPawnOwner());
+	if (Character)
+	{
+		CharacterMovementComponent = Character->GetCharacterMovement();
+	}
+}
+
+void UP1PlayerAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (CharacterMovementComponent == nullptr)
+		return;
+
+	IsFalling = CharacterMovementComponent->IsFalling();
+	Velocity = CharacterMovementComponent->Velocity;
+
+	FVector2D vel = { Velocity.X, Velocity.Y };
+	GroundSpeed = vel.Length();
+
+	ShouldMove = (GroundSpeed > 3.f) && (CharacterMovementComponent->GetCurrentAcceleration().Length() != 0);
+}
--- Source/P1/Animation/P1PlayerAnimInstance.h
+++ Source/P1/Animation/P1PlayerAnimInstance.h
@@ -23,4 +23,13 @@ private:
 	TObjectPtr<class ACharacter> Character;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="References", meta=(AllowPrivateAccess="true"))
 	TObjectPtr<class UCharacterMovementComponent> CharacterMovementComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="EssentialMovementData", meta=(AllowPrivateAccess="true"))
+	FVector Velocity;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="EssentialMovementData", meta=(AllowPrivateAccess="true"))
+	float GroundSpeed;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="EssentialMovementData", meta=(AllowPrivateAccess="true"))
+	bool ShouldMove;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="EssentialMovementData", meta=(AllowPrivateAccess="true"))
+	bool IsFalling;
 };
